718f21303dc8c4aca14e7db6f1ec3139396af929
susumu tomita <oyster880@gmail.com>
2024-01-29 08:01:39 +0900

    format

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -45,11 +45,24 @@ async function main() {
   logger(`Deploying token contract...`);
 
   // Deploy the contract and set Alice as the admin while doing so
-  const contract = await TokenContract.deploy(aliceWallet, alice, 'TokenName', 'TokenSymbol', 18).send().deployed();
-  logger(`Contract successfully deployed at address ${contract.address.toShortString()}`);
+  const contract = await TokenContract.deploy(
+    aliceWallet,
+    alice,
+    'TokenName',
+    'TokenSymbol',
+    18
+  )
+    .send()
+    .deployed();
+  logger(
+    `Contract successfully deployed at address ${contract.address.toShortString()}`
+  );
 
   // Create the contract abstraction and link it to Alice's wallet for future signing
-  const tokenContractAlice = await TokenContract.at(contract.address, aliceWallet);
+  const tokenContractAlice = await TokenContract.at(
+    contract.address,
+    aliceWallet
+  );
 
   // Create a secret and a corresponding hash that will be used to mint funds privately
   const aliceSecret = Fr.random();
@@ -57,17 +70,33 @@ async function main() {
 
   logger(`Minting tokens to Alice...`);
   // Mint the initial supply privately "to secret hash"
-  const receipt = await tokenContractAlice.methods.mint_private(initialSupply, aliceSecretHash).send().wait();
+  const receipt = await tokenContractAlice.methods
+    .mint_private(initialSupply, aliceSecretHash)
+    .send()
+    .wait();
 
   // Add the newly created "pending shield" note to PXE
   const pendingShieldsStorageSlot = new Fr(5); // The storage slot of `pending_shields` is 5.
   const note = new Note([new Fr(initialSupply), aliceSecretHash]);
-  await pxe.addNote(new ExtendedNote(note, alice, contract.address, pendingShieldsStorageSlot, receipt.txHash));
+  await pxe.addNote(
+    new ExtendedNote(
+      note,
+      alice,
+      contract.address,
+      pendingShieldsStorageSlot,
+      receipt.txHash
+    )
+  );
 
   // Make the tokens spendable by redeeming them using the secret (converts the "pending shield note" created above
   // to a "token note")
-  await tokenContractAlice.methods.redeem_shield(alice, initialSupply, aliceSecret).send().wait();
-  logger(`${initialSupply} tokens were successfully minted and redeemed by Alice`);
+  await tokenContractAlice.methods
+    .redeem_shield(alice, initialSupply, aliceSecret)
+    .send()
+    .wait();
+  logger(
+    `${initialSupply} tokens were successfully minted and redeemed by Alice`
+  );
 
   ////////////// QUERYING THE TOKEN BALANCE FOR EACH ACCOUNT //////////////
 
@@ -75,10 +104,14 @@ async function main() {
   // Since we already have a token link, we can simply create a new instance of the contract linked to Bob's wallet
   const tokenContractBob = tokenContractAlice.withWallet(bobWallet);
 
-  let aliceBalance = await tokenContractAlice.methods.balance_of_private(alice).view();
+  let aliceBalance = await tokenContractAlice.methods
+    .balance_of_private(alice)
+    .view();
   logger(`Alice's balance ${aliceBalance}`);
 
-  let bobBalance = await tokenContractBob.methods.balance_of_private(bob).view();
+  let bobBalance = await tokenContractBob.methods
+    .balance_of_private(bob)
+    .view();
   logger(`Bob's balance ${bobBalance}`);
 
   ////////////// TRANSFER FUNDS FROM ALICE TO BOB //////////////
@@ -86,10 +119,15 @@ async function main() {
   // We will now transfer tokens from ALice to Bob
   const transferQuantity = 543n;
   logger(`Transferring ${transferQuantity} tokens from Alice to Bob...`);
-  await tokenContractAlice.methods.transfer(alice, bob, transferQuantity, 0).send().wait();
+  await tokenContractAlice.methods
+    .transfer(alice, bob, transferQuantity, 0)
+    .send()
+    .wait();
 
   // Check the new balances
-  aliceBalance = await tokenContractAlice.methods.balance_of_private(alice).view();
+  aliceBalance = await tokenContractAlice.methods
+    .balance_of_private(alice)
+    .view();
   logger(`Alice's balance ${aliceBalance}`);
 
   bobBalance = await tokenContractBob.methods.balance_of_private(bob).view();
@@ -108,17 +146,31 @@ async function main() {
 
   const mintQuantity = 10_000n;
   logger(`Minting ${mintQuantity} tokens to Bob...`);
-  const mintPrivateReceipt = await tokenContractBob.methods.mint_private(mintQuantity, bobSecretHash).send().wait();
+  const mintPrivateReceipt = await tokenContractBob.methods
+    .mint_private(mintQuantity, bobSecretHash)
+    .send()
+    .wait();
 
   const bobPendingShield = new Note([new Fr(mintQuantity), bobSecretHash]);
   await pxe.addNote(
-    new ExtendedNote(bobPendingShield, bob, contract.address, pendingShieldsStorageSlot, mintPrivateReceipt.txHash),
+    new ExtendedNote(
+      bobPendingShield,
+      bob,
+      contract.address,
+      pendingShieldsStorageSlot,
+      mintPrivateReceipt.txHash
+    )
   );
 
-  await tokenContractBob.methods.redeem_shield(bob, mintQuantity, bobSecret).send().wait();
+  await tokenContractBob.methods
+    .redeem_shield(bob, mintQuantity, bobSecret)
+    .send()
+    .wait();
 
   // Check the new balances
-  aliceBalance = await tokenContractAlice.methods.balance_of_private(alice).view();
+  aliceBalance = await tokenContractAlice.methods
+    .balance_of_private(alice)
+    .view();
   logger(`Alice's balance ${aliceBalance}`);
 
   bobBalance = await tokenContractBob.methods.balance_of_private(bob).view();
